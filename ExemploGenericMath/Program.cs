97d2ef67e0e28cacbf27829580a46279af22e671
Renato Groffe <renato.groffe@yahoo.com.br>
2023-04-23 18:29:21 -0300

    C# 11 + Console Application + Generic Math
    C# 11 + Console Application + Generic Math

---
+++ ExemploGenericMath/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.InteropServices;
+using System.Text.Json;
+using ExemploGenericMath;
+
+Console.WriteLine("***** Testes com C# 11 | Generic Math *****");
+Console.WriteLine($"Versao do .NET em uso: {RuntimeInformation
+    .FrameworkDescription} - Ambiente: {Environment.MachineName}- Kernel: {Environment
+    .OSVersion.VersionString}");
+
+Console.WriteLine();
+
+var transacoes01 = new double[] { 100.05, -10.72, 20.0, -21.0, 37.47 };
+var totalEntradas01 = SaldoBancario.CalcularConsolidado<double>(transacoes01, true);
+var totalSaidas01 = SaldoBancario.CalcularConsolidado<double>(transacoes01, false);
+Console.WriteLine($"{nameof(transacoes01)} = {JsonSerializer.Serialize(transacoes01)} [{transacoes01.GetType().FullName}]");
+Console.WriteLine($"{nameof(totalEntradas01)} = {totalEntradas01:0.00} [{totalEntradas01.GetType().FullName}]");
+Console.WriteLine($"{nameof(totalSaidas01)} = {totalSaidas01:0.00} [{totalSaidas01.GetType().FullName}]");
+
+Console.WriteLine();
+
+var transacoes02 = new decimal[] { 10.0m, -13.0m, 20.82m, -24.53m, 33.17m };
+var totalEntradas02 = SaldoBancario.CalcularConsolidado<decimal>(transacoes02, true);
+var totalSaidas02 = SaldoBancario.CalcularConsolidado<decimal>(transacoes02, false);
+Console.WriteLine($"{nameof(transacoes02)} = {JsonSerializer.Serialize(transacoes02)} [{transacoes02.GetType().FullName}]");
+Console.WriteLine($"{nameof(totalEntradas02)} = {totalEntradas02:0.00} [{totalEntradas02.GetType().FullName}]");
+Console.WriteLine($"{nameof(totalSaidas02)} = {totalSaidas02:0.00} [{totalSaidas02.GetType().FullName}]");
---
+++ ExemploGenericMath/SaldoBancario.cs
@@ -0,0 +1,18 @@
+﻿using System.Numerics;
+
+namespace ExemploGenericMath;
+
+public static class SaldoBancario
+{
+    public static T CalcularConsolidado<T>(T[] transacoes, bool entradas) where T: INumber<T>
+    {
+        var consolidado = T.Zero;
+        foreach (var transacao in transacoes)
+        {
+            if ((entradas & transacao > T.Zero) ||
+                (!entradas & transacao < T.Zero))
+                consolidado += transacao;
+        }
+        return consolidado;
+    }
+}
